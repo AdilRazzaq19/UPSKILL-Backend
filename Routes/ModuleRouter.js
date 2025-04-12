553d620580cc60f260a60d638bbfb04d273de752
Adil Razzaq <i201868@nu.edu.pk>
2025-04-12 15:45:07 +0500

    Fix: update module for admin

--- Routes/ModuleRouter.js
+++ Routes/ModuleRouter.js
@@ -3,14 +3,14 @@ const { createModule, getModules, getModuleById, updateModule,updateModuleName, 
 const { authMiddleware } = require("../middleware/auth.middleware");
 const router = express.Router();
 
-router.post("/create",createModule);
+router.post("/create",authMiddleware,createModule);
 router.get("/getAll", getModules);
 router.get("/getIndividual/:module_id", getModuleById);
 router.get("/getModuleBySection/:section_id",getModulesBySectionId);
 router.put("/update/:id", updateModule);
-router.delete("/delete/:id", deleteModule);
+router.delete("/delete/:id", authMiddleware,deleteModule);
 router.get("/getModuleByUniqueId", getModuleDetailsByUniqueModuleId);
 router.put('/name-update/:moduleId', updateModuleName);
 router.get("/getAllModulesForAdmin", authMiddleware,getAllModulesForAdmin);
-router.put("/updateModuleforAdmin/:moduleId", updateModuleforAdmin);
+router.put("/updateModuleforAdmin/:moduleId",authMiddleware, updateModuleforAdmin);
 module.exports = router;
